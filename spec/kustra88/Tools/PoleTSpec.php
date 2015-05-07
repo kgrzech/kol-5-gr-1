54b754c9e827e793f8b80f628388fa0272990916
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-05-07 11:24:58 +0200

    kustra88/pole-trojkata: implementacja metod

--- spec/kustra88/Tools/PoleTSpec.php
+++ spec/kustra88/Tools/PoleTSpec.php
@@ -11,4 +11,13 @@ class PoleTSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kustra88\Tools\PoleT');
     }
+    function it_should_have_setter_and_gettter()
+    {
+    	$this->setA(2)->getA()->shouldReturn(2);
+    	$this->setH(2)->getH()->shouldReturn(2);
+    }
+    function it_should_calculate_field_triangle()
+    {
+    	$this->setA(2)->setH(2)->triangle()->shouldReturn(2.0);
+    }
 }
--- src/kustra88/Tools/PoleT.php
+++ src/kustra88/Tools/PoleT.php
@@ -4,4 +4,29 @@ namespace kustra88\Tools;
 
 class PoleT
 {
+    private $a;
+    private $h;
+    public function setA($a)
+    {
+        $this->a = $a;
+        return $this;
+    }
+    public function setH($h)
+    {
+        $this->h = $h;
+        return $this;
+    }
+    public function getA()
+    {
+        return $this->a;
+    }
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function triangle()
+    {
+        return $this->a * $this->h * 0.5;
+    }
 }

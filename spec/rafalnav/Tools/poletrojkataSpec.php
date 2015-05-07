67cc0bca576c573cf16a6ca529c05e7bae2d4a5f
Rafal Nawalany <rafalnav@gmail.com>
2015-05-07 11:15:26 +0200

    Rafal/nav/poletrojkata: phpspec

---
+++ spec/rafalnav/Tools/poletrojkataSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\rafalnav\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class poletrojkataSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('rafalnav\Tools\poletrojkata');
+    }
+}

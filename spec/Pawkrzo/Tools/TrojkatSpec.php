a05d519bb0466a7d76db464dcec45c6ede30bff9
pawkrzo <pawkrzo@gmail.com>
2015-05-07 11:35:24 +0200

    PAwkrzo/trojakt: trojakat cd

--- spec/Pawkrzo/Tools/TrojkatSpec.php
+++ spec/Pawkrzo/Tools/TrojkatSpec.php
@@ -7,8 +7,13 @@ use Prophecy\Argument;
 
 class TrojkatSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_should_have_setter_and_gettter()
     {
-        $this->shouldHaveType('Pawkrzo\Tools\Trojkat');
+        $this->setA(5.0)->getA()->shouldReturn(5.0);
+        $this->setH(2.0)->getH()->shouldReturn(2.0);
     }
+    function it_should_calculate_triangle()
+{
+    $this->setA(5.0)->setH(2.0)->triangle()->shouldReturn(5.0);
+}
 }
--- src/Pawkrzo/Tools/Trojkat.php
+++ src/Pawkrzo/Tools/Trojkat.php
@@ -4,4 +4,40 @@ namespace Pawkrzo\Tools;
 
 class Trojkat
 {
+
+   private $a;
+    private $h;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setH($h)
+    {
+        $this->h = $h;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function triangle()
+    {
+    return $this->a * $this->h * 0.5 ;    }
+    
+    public function setB($argument1)
+    {
+        // TODO: write logic here
+    }
 }

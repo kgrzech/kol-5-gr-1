3c18adf1cc712fa3e4afc6c9f97040e7cc7cfebc
majdan <majdanski.dawid@gmail.com>
2015-05-07 11:30:52 +0200

    Majdan: metoda obliczajaca pole trojkata

--- spec/Majdan/Tools/PoletrojkataSpec.php
+++ spec/Majdan/Tools/PoletrojkataSpec.php
@@ -11,4 +11,15 @@ class PoletrojkataSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Majdan\Tools\Poletrojkata');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+ 
+    function it_should_calculate_area_of_triangle()
+    {
+        $this->setA(4)->setB(3)->area_of_triangle()->shouldReturn(6);
+    }
 }
--- src/Majdan/Tools/Poletrojkata.php
+++ src/Majdan/Tools/Poletrojkata.php
@@ -4,4 +4,38 @@ namespace Majdan\Tools;
 
 class Poletrojkata
 {
+    private $a;
+    private $b;
+   
+    
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+
+    public function area_of_triangle()
+    {
+        
+        return ($this->a / 2) * $this->b;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
 }

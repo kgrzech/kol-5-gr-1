16e5f4de42d5ea4f4356977df54ce5022f8b04ad
kgrzech <ambros1112@hotmail.com>
2015-05-07 12:10:32 +0200

    trojkat kgrzech

--- spec/kgrzech/Tools/trojkatSpec.php
+++ spec/kgrzech/Tools/trojkatSpec.php
@@ -11,4 +11,9 @@ class trojkatSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kgrzech\Tools\trojkat');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setH(2)->getH()->shouldReturn(2);
+    }
 }
--- src/kgrzech/Tools/trojkat.php
+++ src/kgrzech/Tools/trojkat.php
@@ -4,4 +4,48 @@ namespace kgrzech\Tools;
 
 class trojkat
 {
+
+     private $a;
+     private $h;
+     private $c;
+
+    public function setC($c)
+    {
+        $this->a = $c;
+
+        return $this;
+    }
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setH($h)
+    {
+        $this->h = $h;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+     public function getC()
+    {
+        return $this->c;
+    }
+
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function pole()
+    {
+    return $this->a * $this->h * $this ->c;
+    
+    }
 }
